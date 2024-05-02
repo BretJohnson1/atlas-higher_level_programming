f2aec73d5e385ecdaa52a983b581565de5a9a08b
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-02 17:25:02 +0000

    is it uppercase?

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= char <= 122:
+            char = char(ord(char) - 32)
+            print("{}".format(char), end="")
+            print("")
