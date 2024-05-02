160637511f985e52cc402d75b3643e6e85e151f5
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-02 18:06:06 +0000

    adding infinitely

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    from sys import argv
     sum = 0
-    for arg in sys.argv[1:]:
-        sum += int(arg)
-        print(sum)
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
