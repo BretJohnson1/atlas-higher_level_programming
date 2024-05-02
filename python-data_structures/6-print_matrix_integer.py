038c4a7346e5739107a65e301815697a70a8df74
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-02 18:46:49 +0000

    the matrix is real

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        last_element = row[-1]
-        for element in row:
-            if element != last_element:
-                print("{:d} ".format(element), end='')
-            else:
-                print("{;d}".format(element), end='')
-                print()
+    for i in matrix:
+        print(" ".join("{:d}".format(num) for num in i))
