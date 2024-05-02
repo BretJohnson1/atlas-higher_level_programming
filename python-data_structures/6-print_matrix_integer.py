bfaca6e56665d77afce0fb61765b6be0dfd833b2
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-02 18:45:45 +0000

    printing the matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        last_element = row[-1]
+        for element in row:
+            if element != last_element:
+                print("{:d} ".format(element), end='')
+            else:
+                print("{;d}".format(element), end='')
+                print()
