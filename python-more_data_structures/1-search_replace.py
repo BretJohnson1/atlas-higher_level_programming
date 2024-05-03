32cdb69e80d3d5ff6aa9384aa8a2a0171f829f6b
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-03 13:51:31 +0000

    rework my for loop

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-            return new_list
+    new_list = my_list[:]
+    list_len = len(my_list)
+
+    for i in range(list_len):
+        if(my_list[i] == search):
+            new_list[i] = replace
+    return new_list
