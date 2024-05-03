108836a464b3192aaf9a38b5c4d489b87c23af63
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-03 13:52:33 +0000

    typo fix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     list_len = len(my_list)
 
     for i in range(list_len):
-        if(my_list[i] == search):
+        if (my_list[i] == search):
             new_list[i] = replace
     return new_list
