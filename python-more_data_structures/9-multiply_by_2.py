fedb8deb743ecb00e60747c005db22699cd9ebb4
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-06 15:43:47 +0000

    resetting values in file #9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key in a_dictionary:
-        new_dictionary[key] += new_dictionary[key]
-        return new_dictionary
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
+    return new_dictionary
