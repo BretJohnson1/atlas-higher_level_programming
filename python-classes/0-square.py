059be422d41aaa6822859783c83851f1264bafa4
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-13 15:47:32 +0000

    making a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Defines a class called square """
+
+class square:
+    """ This class defines a square """
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+In this project, I'll be learnig about python classes and how they are used
