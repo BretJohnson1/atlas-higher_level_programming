74aa0278db7c915688b6b06d13c26266f5358623
BretJohnson1 <bretjohnson004@gmail.com>
2024-05-13 16:33:26 +0000

    fixing typos

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""For my square"""
-
+""" For my square """
 
 class Square:
-    """ Initializing Class for my square."""
+    """This class defines a square"""
     def __init__(self, size=0):
-        """ New sqaure
-
-
-        Args:
-        size (int): Size of square"""
-        if not isinstance(size, int):
+        """This function defines a square"""
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Calculates area of a square"""
+        return (self.__size**2)
