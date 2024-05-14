5b6efe5ed37da01adc50a337bd4ce24eb66c4fd0
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-05-14 13:51:36 -0500

    this is a pain

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" This module contains a class defined as Rectangle """
+"""This module contains a class defined as Rectangle"""
 
 
 class Rectangle:
-    """ This is a class that defines a Rectangle """
+    """This is a class that defines a Rectangle"""
     def __init__(self, width=0, height=0):
-        """ This method initializes its properties """
+        """This method initializes its properties"""
         if not isinstance(width, int):
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
@@ -17,57 +17,57 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = height
 
-@property
-def width(self):
-    """ This method defines its width """
-    return self.__width
+    @property
+    def width(self):
+        """This method defines its width"""
+        return self.__width
 
-@width.setter
-def width(self, value):
-    """ This method sets value to its width """
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    elif value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
+    @width.setter
+    def width(self, value):
+        """This method sets value to its width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-@property
-def height(self):
-    """ This method defines its height """
-    return self.__height
+    @property
+    def height(self):
+        """This method defines its height"""
+        return self.__height
 
-@height.setter
-def height(self, value):
-    """ This method sets a value to its height """
-    if not isinstance(value, int):
-        raise TypeError("height must be an integer")
-    elif value < 0:
-        raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        """This method sets value to its height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """ This public method returns the area of the rectangle """
+        """This public method returns the area of the Rectangle"""
         return self.__height * self.__width
 
     def perimeter(self):
-        """ This public method returns the perimeter of the rectangle """
+        """This public method returns the perimeter of the Rectangle"""
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        """ This method is the string representation of the Rectangle """
+        """This method is the string representation of the Rectangle"""
         string = ""
         if self.width > 0 and self.height > 0:
             for row in range(self.height):
-                for col in range(slef.width):
+                for col in range(self.width):
                     string += '#'
-                    if row < self.height - 1:
-                        string += '\n'
-                        return string
+                if row < self.height - 1:
+                    string += '\n'
+            return string
         else:
             return string
 
-def __repr__(self):
-    """ This method returns the string representation of new object """
-    return f"Rectangle({self.__width}, {self.__height})"
+    def __repr__(self):
+        """This method returns the string representation of new object"""
+        return f"Rectangle({self.__width}, {self.__height})"
