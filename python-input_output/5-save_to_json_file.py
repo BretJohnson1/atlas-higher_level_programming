982a0c430f6feebb8c85ad3398770484726483d9
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-05-20 13:58:22 -0500

    reworking to save json darulo

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines a function that writes an object to a text file
-using JSON representation
-"""
+''' Function that writes an Object to a text file, using
+a JSON representation '''
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """This function writes an object to a text file using JSON rep"""
-    with open(filename, mode='w', encoding="utf-8") as f:
-        json.dump(my_obj, f)
+    ''' Function that writes an Object to a text file, using
+    a JSON representation '''
+    with open(filename, 'w', encoding='utf-8') as holby:
+        json.dump(my_obj, holby)
