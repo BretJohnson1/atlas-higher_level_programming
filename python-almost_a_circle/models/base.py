745c7ec3064eabfdd2b7b9594762b3d6316d621b
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-05-28 19:15:36 -0500

    fixing inline spaces

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """ Function to make into json file"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+            return  "[]"
         else:
             return json.dumps(list_dictionaries)
 
