e41b93d55d95321fd18129ea78a793a4534b1ce4
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-06-17 12:47:14 -0500

    reworking select states file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a function that establishes a connection to a SQL server"""
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
 
 import MySQLdb
 import sys
 
 
-def sql_connect(usr, pw, db_name):
-    """This function imports a database to use"""
+def list_states():
+    """ List the states """
 
-    """Establish connection"""
-    db = MySQLdb.connect(host="localhost",
-                         user=usr,
-                         port=3306,
-                         passwd=pw,
-                         database=db_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-    """Create cursor object"""
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
 
-    """Access the queried data to print"""
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    """Close the connection and cursor object"""
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
+    list_states()
