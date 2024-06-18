c900535a1ff225227ac678f665db37d47896281a
Your Name <you@example.com>
2024-06-18 20:05:09 +0000

    #2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a function that creates a connection to a SQL server"""
+""" Write a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument. """
 
 import MySQLdb
 import sys
 
 
-def match_state(username, password, dbase, searched):
-    """This function imports a database to use"""
+def list_states_by_name():
+    """ List the state specified """
 
-    """Establish connection"""
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         database=dbase)
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-    """Create cursor object"""
     cur = db.cursor()
-    cur.execute("SELECT * FROM {0} ORDER BY {0}.id ASC;".format(searched))
 
-    """Access the queried data to print"""
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                 '{}' ORDER BY id".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == searched:
-            print(row)
+        print(row)
 
-    """Close the connection and cursor object"""
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    match_state(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    list_states_by_name()
