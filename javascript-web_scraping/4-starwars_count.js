b6122b85c02e79715ba1177ae1e4b948e03d07b1
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-07-26 10:35:47 -0500

    slight rework of count process

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const characterId = '18/';
-const filmApiUrl = process.argv[2];
-const characterApiUrl = 'https://swapi-api.hbtn.io/api/people/';
+const url = process.argv[2];
 
-request(filmApiUrl, function (err, response, body) {
-  if (!err && response.statusCode === 200) {
-    // Convert the data to JSON
-    const films = JSON.parse(body).results;
-    // Initialize movieCount
-    let movieCount = 0;
-    // Iterating over each film to find matching character id
-    films.forEach(function (film) {
-      // Find the characters for each movie
-      const characters = film.characters;
-      // Check if the characterId exists in the characters array
-      if (characters.includes(characterApiUrl + characterId)) {
-        movieCount++;
+request(url, function (err, response, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    const json = JSON.parse(data).results;
+    let countWedge = 0;
+    for (const movie in json) {
+      const characters = json[movie].characters;
+      for (const person in characters) {
+        if (characters[person].includes('/18/')) {
+          countWedge += 1;
+        }
       }
-    });
-    console.log(movieCount);
+    }
+    console.log(countWedge);
   }
 });
